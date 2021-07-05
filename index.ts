7db5946ae71a3710781601e1db77c603044de17f
Parsifal <67693767+Parsifal@users.noreply.github.com>
2021-07-06 02:01:26 +0300

    Bug fixes Fixed an issue where the method could be triggered when there is no loop

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
-import * as https from "https";
+import { get } from "https";
 
 const token = "";
 
 class DeadInside {
-  private cycle!: NodeJS.Timeout;
+  private cycle?: NodeJS.Timeout;
   private number = 1000;
 
   constructor(private readonly token: string, private readonly interval = 60 * 1000) {}
 
   startStatusChangeCycle() {
-    console.log("The cycle has started");
     this.endStatusChangeCycle();
+    this.createCycle();
+    console.log("The cycle has started");
+  }
+
+  private createCycle() {
     this.cycle = setInterval(() => {
       if (this.number < 0) this.number = 1000;
       const status = `${this.number} - 7 = ${this.number - 7}`;
-      https.get(`https://api.vk.com/method/status.set?text=${status}&access_token=${this.token}&v=5.130`, (response) => {
+      get(`https://api.vk.com/method/status.set?text=${status}&access_token=${this.token}&v=5.130`, (response) => {
         if (response.statusCode !== 200) throw new Error("The response code is not 200");
         console.log(`${this.number} - 7 = ${this.number - 7}`);
       });
@@ -23,7 +27,7 @@ class DeadInside {
   }
 
   endStatusChangeCycle() {
-    clearInterval(this.cycle);
+    if (this.cycle) clearInterval(this.cycle);
     console.log("Cycle stopped");
   }
 }
