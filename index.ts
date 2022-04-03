cd14d44cf633793ac101880a7161ded221e73578
Parsifal <67693767+Parsifal@users.noreply.github.com>
2022-04-03 19:25:00 +0300

    Refactoring

--- index.ts
+++ index.ts
@@ -3,33 +3,37 @@ import { get } from "https";
 const token = "";
 
 class DeadInside {
-  private cycle?: NodeJS.Timeout;
+  private timer?: NodeJS.Timer;
   private number = 1000;
 
-  constructor(private readonly token: string, private readonly interval = 60 * 1000) {}
+  constructor(private readonly token: string, private readonly interval = 2 * 1000) {}
 
-  startStatusChangeCycle() {
-    this.endStatusChangeCycle();
-    this.createCycle();
+  start() {
+    this.stop();
+    this.createTimer();
     console.log("The cycle has started");
   }
 
-  private createCycle() {
-    this.cycle = setInterval(() => {
-      if (this.number < 0) this.number = 1000;
-      const status = `${this.number} - 7 = ${this.number - 7}`;
-      get(`https://api.vk.com/method/status.set?text=${status}&access_token=${this.token}&v=5.131`, (response) => {
-        if (response.statusCode !== 200) throw new Error("The response code is not 200");
-        console.log(`${this.number} - 7 = ${this.number - 7}`);
-      });
-      this.number -= 7;
+  private createTimer() {
+    this.timer = setInterval(() => {
+      try {
+        if (this.number < 0) this.number = 1000;
+        const status = `${this.number} - 7 = ${this.number - 7}`;
+        get(`https://api.vk.com/method/status.set?text=${status}&access_token=${this.token}&v=5.81`, (response) => {
+          if (response.statusCode !== 200) throw new Error("The response code is not 200");
+          console.log(`${this.number} - 7 = ${this.number - 7}`);
+        });
+        this.number -= 7;
+      } catch (e) {
+        this.stop();
+      }
     }, this.interval);
   }
 
-  endStatusChangeCycle() {
-    if (this.cycle) clearInterval(this.cycle);
+  stop() {
+    if (this.timer) clearInterval(this.timer);
     console.log("Cycle stopped");
   }
 }
 
-new DeadInside(token).startStatusChangeCycle();
+new DeadInside(token).start();
